6cd2552a68d812decd02e277b49378ef04d45cd5
Mihai Dinca <mdinca@suse.de>
2018-04-13 21:52:47 +0200

    Use separate data folders

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import pandas
 import numpy as np
@@ -68,34 +69,34 @@ def create_model(load=False):
 
 
 def main():
-    images = []
-    angles = []
 
     def fun(path):
         assert path
-        image = cv2.imread('./{0}/IMG/'.format(DATA_FOLDER) + path.split('/')[-1])
+        image = cv2.imread('/'.join(['.'] + path.split('/')[-4:]))
         # assert image.shape == (160, 320, 3)
         return image
 
-    for chunk in pandas.read_csv(
-        './%s/driving_log.csv' % DATA_FOLDER,
-        names=['center', 'left', 'right', 'angles', 'x', 'y', 'speed'],
-        chunksize=1000
-    ):
-        center_images_chunk = chunk.loc[:, 'center']
+    images = []
+    angles = []
+    data_root = './%s' % DATA_FOLDER
+    for item in os.listdir(data_root):
+        for chunk in pandas.read_csv(
+            '/'.join([data_root, item, 'driving_log.csv']),
+            names=['center', 'left', 'right', 'angles', 'x', 'y', 'speed'],
+            chunksize=1000
+        ):
+            images += chunk.loc[:, 'center'].tolist()
+            angles += chunk.loc[:, 'angles'].tolist()
 
-        images += center_images_chunk.tolist()
-        angles += chunk.loc[:, 'angles'].tolist()
+            correction = 0.5
+            left_images_chunk = chunk.loc[:, 'left']
+            right_images_chunk = chunk.loc[:, 'right']
 
-        correction = 0.3 * chunk.loc[:, 'angles']
-        left_images_chunk = chunk.loc[:, 'left']
-        right_images_chunk = chunk.loc[:, 'right']
+            images += left_images_chunk.tolist()
+            angles += (correction + chunk.loc[:, 'angles']).tolist()
 
-        images += left_images_chunk.tolist()
-        angles += (correction + chunk.loc[:, 'angles']).tolist()
-
-        images += right_images_chunk.tolist()
-        angles += (-correction + chunk.loc[:, 'angles']).tolist()
+            images += right_images_chunk.tolist()
+            angles += (-correction + chunk.loc[:, 'angles']).tolist()
 
     from sklearn.model_selection import train_test_split
     train_images, valid_images, train_angles, valid_angles = train_test_split(images, angles, test_size=0.2, random_state=13)
@@ -127,13 +128,13 @@ def main():
     train_generator = generator(train_images, train_angles, batch_size=32)
     valid_generator = generator(valid_images, valid_angles, batch_size=32)
 
-    model = create_model(True)
+    model = create_model()
     history = model.fit_generator(
         train_generator,
-        samples_per_epoch=32*100,
+        samples_per_epoch=6*len(train_images),
         validation_data=valid_generator,
-        nb_val_samples=len(valid_images),
-        nb_epoch=7,
+        nb_val_samples=6*len(valid_images),
+        nb_epoch=3,
         verbose=1)
 
     model.save('model.h5')
