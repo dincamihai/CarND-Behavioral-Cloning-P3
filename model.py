1a91e31cfa52a5a8f180759387b61311bdd165be
Mihai Dinca <mdinca@suse.de>
2018-04-21 15:14:21 +0200

    Model able to drive ok on track2 with only track1 training

--- model.py
+++ model.py
@@ -18,44 +18,58 @@ from sklearn.utils import shuffle
 
 MODEL_FILE = 'model.h5'
 # ALLOWED_PATTERN = 'data/*[!-track2]'
-ALLOWED_PATTERN = 'data/*'
+ALLOWED_PATTERN = 'data.v1/*'
 
 
 def create_model(load=False):
+    inputs = Input(shape=(160, 320, 3))
+    main = Cropping2D(cropping=((75,20), (0,0)), input_shape=(160, 320, 3))(inputs)
+    main = Lambda(lambda x: x/255.0 - 0.5)(main)
+    main = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='valid')(main)
+
+    right = Convolution2D(24, 5, 5, border_mode='valid')(main)
+    right = AveragePooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(right)
+    right = Activation('relu')(right)
+    right = Dropout(0.3)(right)
+
+    right = Convolution2D(48, 3, 3, border_mode='valid')(main)
+    right = AveragePooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(right)
+    right = Activation('relu')(right)
+    right = Dropout(0.5)(right)
+
+    right = Convolution2D(48, 2, 2, border_mode='valid')(right)
+    right = AveragePooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(right)
+    right = Activation('relu')(right)
+    right = Dropout(0.5)(right)
+
+    left = Convolution2D(48, 5, 5, border_mode='valid')(main)
+    left = MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(left)
+    left = Activation('relu')(left)
+    left = Dropout(0.5)(left)
+
+    left = Convolution2D(48, 2, 2, border_mode='valid')(left)
+    left = AveragePooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(left)
+    left = Activation('relu')(left)
+    left = Dropout(0.5)(left)
+
+    main = merge([Flatten()(left), Flatten()(right)], mode='concat')
+
+    main = Dense(50, W_regularizer=l2(0.009))(main)
+    main = Activation('relu')(main)
+
+    main = Dense(20, W_regularizer=l2(0.009))(main)
+    main = Activation('relu')(main)
+
+    main = Dense(1)(main)
+    model = Model(input=inputs, output=main)
     if load:
-        return load_model(MODEL_FILE)
-    else:
-        inputs = Input(shape=(160, 320, 3))
-        main = Cropping2D(cropping=((75,20), (0,0)), input_shape=(160, 320, 3))(inputs)
-        main = Lambda(lambda x: x/255.0 - 0.5)(main)
-        main = MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid', dim_ordering='tf')(main)
-
-        main = Convolution2D(24, 2, 2, border_mode='valid')(main)
-        main = AveragePooling2D(pool_size=(3, 3), strides=None, border_mode='valid', dim_ordering='tf')(main)
-        main = Activation('relu')(main)
-        main = Dropout(0.5)(main)
-
-        main = Convolution2D(48, 2, 2, border_mode='valid')(main)
-        main = AveragePooling2D(pool_size=(3, 3), strides=None, border_mode='valid', dim_ordering='default')(main)
-        main = Activation('relu')(main)
-        main = Dropout(0.5)(main)
-
-        main = Flatten()(main)
-
-        main = Dense(50, W_regularizer=l2(0.7))(main)
-        main = Activation('relu')(main)
-
-        main = Dense(7, W_regularizer=l2(0.5))(main)
-        main = Activation('relu')(main)
-
-        main = Dense(1)(main)
-        model = Model(input=inputs, output=main)
-        model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-        return model
+        model.load_weights(MODEL_FILE)
+    model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+    return model
 
 
 def main():
-    correction = 0.6
+    correction = 0.4
 
     def fun(path):
         assert path
@@ -72,22 +86,28 @@ def main():
             names=['center', 'left', 'right', 'angles', 'x', 'y', 'speed'],
             chunksize=1000
         ):
-            images += chunk.loc[:, 'center'].tolist()
-            angles += chunk.loc[:, 'angles'].tolist()
+            center_images = chunk.loc[:, 'center']
+            center_angles = chunk.loc[:, 'angles']
 
-            left_images_chunk = chunk.loc[:, 'left']
-            right_images_chunk = chunk.loc[:, 'right']
+            left_images = chunk.loc[:, 'left']
+            left_angles = correction + center_angles
 
-            images += left_images_chunk.tolist()
-            angles += (correction + chunk.loc[:, 'angles']).tolist()
+            right_images = chunk.loc[:, 'right']
+            right_angles = -correction + center_angles
 
-            images += right_images_chunk.tolist()
-            angles += (-correction + chunk.loc[:, 'angles']).tolist()
+            images += center_images.tolist()
+            angles += center_angles.tolist()
+
+            images += left_images.tolist()
+            angles += left_angles.tolist()
+
+            images += right_images.tolist()
+            angles += right_angles.tolist()
 
     from sklearn.model_selection import train_test_split
     train_images, valid_images, train_angles, valid_angles = train_test_split(images, angles, test_size=0.2, random_state=13)
 
-    def generator(images, angles, batch_size=32, augment=False):
+    def generator(images, angles, batch_size=32):
         num_samples = len(images)
         while 1: # Loop forever so the generator never terminates
             shuffle(images, angles)
@@ -100,31 +120,32 @@ def main():
                 for path, angl in zip(batch_images, batch_angles):
                     image = fun(path)
                     if image is None:
-                        print("Skipping image: %s" % image)
-                        continue
-                    if (not augment) and not ('center' in path):
+                        print("Skipping image: %s" % path)
                         continue
                     gen_images.append(image)
                     gen_angles.append(angl)
-                    if augment:
-                        gen_images.append(np.fliplr(image))
-                        gen_angles.append(-angl)
+                    gen_images.append(np.fliplr(image))
+                    gen_angles.append(-angl)
 
                 # trim image to only see section with road
                 X_train = np.array(gen_images)
                 y_train = np.array(gen_angles)
                 yield shuffle(X_train, y_train)
 
-    train_generator = generator(train_images, train_angles, batch_size=128, augment=True)
-    valid_generator = generator(valid_images, valid_angles, batch_size=128, augment=True)
+    train_generator = generator(train_images, train_angles, batch_size=128)
+    valid_generator = generator(valid_images, valid_angles, batch_size=128)
 
     model = create_model()
+
+    num_train_images = (2 * len(train_images) / 128)
+    samples_per_epoch = num_train_images * 128
+
     history = model.fit_generator(
         train_generator,
-        samples_per_epoch=len(train_images)/10,
+        samples_per_epoch=samples_per_epoch,
         validation_data=valid_generator,
-        nb_val_samples=len(valid_images)/10,
-        nb_epoch=5,
+        nb_val_samples=samples_per_epoch*0.2,
+        nb_epoch=3,
         verbose=1)
 
     model_file = 'model.h5-{0}-{1}'.format(datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S"), correction)
